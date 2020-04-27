c71fcbf40a0ae65c8f5f43145519023c2b6a32df
swagpenny <swagpenny000@gmail.com>
2020-04-27 16:30:43 -0400

    small change

---
+++ pennywang/server/build/precache-manifest.ba9623d2fee930ae11db3fb3217e5e96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7016db4e7c766fc53ea0e87ba28eb733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6cfa8965e3d1d3c2125",
+    "url": "/static/css/main.70d2f3c4.chunk.css"
+  },
+  {
+    "revision": "d84dcb8aff1e2edf295d",
+    "url": "/static/js/2.2492280e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2492280e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cfa8965e3d1d3c2125",
+    "url": "/static/js/main.4ff86249.chunk.js"
+  },
+  {
+    "revision": "c4f9307b8cb6f814d601",
+    "url": "/static/js/runtime-main.e415bcfc.js"
+  },
+  {
+    "revision": "492f69850e2da551f2d974bb2384c6c6",
+    "url": "/static/media/fake-ad.492f6985.png"
+  },
+  {
+    "revision": "f82579e9c39573da9cce48fb78f47d34",
+    "url": "/static/media/like.f82579e9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8619ce79eba604f68d6b62812e0ccedb",
+    "url": "/static/media/red.8619ce79.png"
+  }
+]);
